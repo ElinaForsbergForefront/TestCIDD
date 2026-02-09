9bd5b6ef18c95bff71d739c3760572ffa084872b
Elina Forsberg <elina.forsberg@sveaskog.se>
2026-02-09 11:04:04 +0100

    test test

---
+++ ClassLibrary1/ClassLibrary1.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace ClassLibrary1.Tests;
+
+public class ExampleTests
+{
+    [Test]
+    public void TwoPlusTwo_IsFour()
+    {
+        Assert.That(2 + 2, Is.EqualTo(4));
+    }
+}
